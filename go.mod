7a6f65f5e9847c92539d870e768b6c8dbbe75e7c
ZhongDuying <zhongduying@163.com>
2021-06-06 00:40:22 +0800

    数据库基础操作测试完毕

---
+++ go.mod
@@ -0,0 +1,5 @@
+module geektime
+
+go 1.16
+
+require github.com/jmoiron/sqlx v1.3.4
--- week02/main.go
+++ week02/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type User struct {
+	Id			int32	`db:"id"`
+	Name		string	`db:"name"`
+	Password 	string	`db:"password"`
+	Phone		string	`db:"phone"`
+	Email		string	`db:"email"`
+	CreateTime	string	`db:"create_time"`
+	UpdateTime	string	`db:"update_time"`
+}
 
 func main() {
-	fmt.Println("Test github: Hello World!")
+	db, err :=sqlx.Connect("mysql", "geek:geek@tcp(localhost:3306)/geektime")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	query := "select * from user where id = ?"
+
+	user := User{}
+	id := 1
+	err = db.Get(&user, query, id)
+	fmt.Printf("db.Get(%v): \n\t" +
+		"user = %v\n\t" +
+		"err : %v\n\n",
+		id, user, err)
+
+	user = User{}
+	id = 2
+	err = db.Get(&user, query, id)
+	fmt.Printf("db.Get(%v): \n\t" +
+		"user = %v\n\t" +
+		"err : %v\n\n",
+		id, user, err)
+
 }
