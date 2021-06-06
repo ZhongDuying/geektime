19f03a58fab3890e4f9cbb90d7c90edce10e3bdf
ZhongDuying <zhongduying@163.com>
2021-06-06 10:02:11 +0800

    第02周作业完成

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module geektime
 
 go 1.16
 
-require github.com/jmoiron/sqlx v1.3.4
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/pkg/errors v0.9.1
+)
--- week02/main.go
+++ week02/main.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	xerrors "github.com/pkg/errors"
 )
+
+
+////////////////////////////////////////////////////////////////
+// dao层
 
 type User struct {
 	Id			int32	`db:"id"`
@@ -16,7 +21,141 @@ type User struct {
 	UpdateTime	string	`db:"update_time"`
 }
 
+func DbConnect(address, dbname,  username, password string) (*sqlx.DB, error) {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, address, dbname)
+	db, err := sqlx.Connect("mysql", dataSourceName)
+	if err != nil {
+		return nil, xerrors.Wrap(err, "db connect fail")
+	}
+
+	return db, nil
+}
+
+func DbGetUserInfo(db *sqlx.DB, id int32) (*User, error) {
+	user := &User{}
+	query := "select * from user where id = ?"
+	err := db.Get(user, query, id)
+	if err != nil {
+		return nil, xerrors.Wrap(err, "user not found by id")
+	}
+
+	return user, nil
+}
+
+////////////////////////////////////////////////////////////////
+// 业务处理层
+
+type SysInfo struct {
+	dbConf	*DbConfig
+	db		*sqlx.DB
+	// other info...
+}
+
+type DbConfig struct {
+	address		string
+	dbname		string
+	username	string
+	password	string
+}
+
+type SysProc struct {
+	SysInfo
+}
+
+func (sysProc *SysProc) init(dbConf *DbConfig) error {
+	db, err := DbConnect(dbConf.address,
+		dbConf.dbname,
+		dbConf.username,
+		dbConf.password)
+	if err != nil {
+		return xerrors.WithMessage(err, "sys init fail")
+	}
+
+	sysProc.dbConf = dbConf
+	sysProc.db = db
+	return nil
+}
+
+func (sysProc *SysProc) userLogin(userid int32) error {
+	user, err := DbGetUserInfo(sysProc.db, userid)
+	if err != nil {
+		return xerrors.WithMessage(err, "user login fail")
+	}
+
+	// 其他业务处理...
+	fmt.Printf("User Info : \n\t%v\n", user)
+
+	return nil
+}
+
+////////////////////////////////////////////////////////////////
+// 业务逻辑测试
+func testSucc() {
+	dbConf := &DbConfig{"localhost:3306", "geektime", "geek", "geek"}
+	sys := &SysProc{}
+	err := sys.init(dbConf)
+	if err != nil {
+		fmt.Println("Init")
+		fmt.Printf("original error: %T %v\n",
+			xerrors.Cause(err), xerrors.Cause(err))
+		fmt.Printf("stack trace:\n%+v", err)
+	}
+
+	err = sys.userLogin(1)
+	if err != nil {
+		fmt.Println("Login")
+		fmt.Printf("original error: %T %v\n",
+			xerrors.Cause(err), xerrors.Cause(err))
+		fmt.Printf("stack trace:\n%+v", err)
+	}
+}
+
+
+func testInitFail() {
+	dbConf := &DbConfig{"localhost:3306", "geektime", "geek", "geek1"}
+	sys := &SysProc{}
+	err := sys.init(dbConf)
+	if err != nil {
+		fmt.Println("Init")
+		fmt.Printf("original error: %T %v\n",
+			xerrors.Cause(err), xerrors.Cause(err))
+		fmt.Printf("stack trace:\n%+v", err)
+	}
+}
+
+func testLoginFail() {
+	dbConf := &DbConfig{"localhost:3306", "geektime", "geek", "geek"}
+	sys := &SysProc{}
+	err := sys.init(dbConf)
+	if err != nil {
+		fmt.Println("Init")
+		fmt.Printf("original error: %T %v\n",
+			xerrors.Cause(err), xerrors.Cause(err))
+		fmt.Printf("stack trace:\n%+v", err)
+	}
+
+	err = sys.userLogin(2)
+	if err != nil {
+		fmt.Println("Login")
+		fmt.Printf("original error: %T %v\n",
+			xerrors.Cause(err), xerrors.Cause(err))
+		fmt.Printf("stack trace:\n%+v", err)
+	}
+}
+
 func main() {
+	fmt.Printf("\n\nTEST SUCCESS:\n")
+	testSucc()
+
+	fmt.Printf("\n\nTEST INIT FAIL:\n")
+	testInitFail()
+
+	fmt.Printf("\n\nTEST LOGIN FAIL:\n")
+	testLoginFail()
+}
+
+
+func main1() {
 	db, err :=sqlx.Connect("mysql", "geek:geek@tcp(localhost:3306)/geektime")
 	if err != nil {
 		fmt.Println(err)
